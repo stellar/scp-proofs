d992ebf0a423a1fdc233cf72f63744aa633c707c
Giuliano Losa <giuliano@losa.fr>
2019-09-11 10:15:30 -0700

    update comments

--- FBA.thy
+++ FBA.thy
@@ -5,39 +5,46 @@ imports Main
 begin
 
 definition project where 
-  "project slices S n \<equiv> {Sl \<inter> S | Sl . Sl \<in> slices n}"
+  "project slices S n \<equiv> {Sl \<inter> S | Sl . Sl \<in> slices n}" 
+  \<comment> \<open>Projecting on @{term S} is the same as deleting the complement of @{term S}, where deleting is understood as in the Stellar Whitepaper.\<close>
 
 locale FBAS =
   fixes slices :: "'node \<Rightarrow> 'node set set" \<comment> \<open>the quorum slices\<close>
     and W :: "'node set" \<comment> \<open>the well-behaved nodes\<close>
-  assumes slices_ne:"\<And>p . p \<in> W \<Longrightarrow> slices p \<noteq> {}"
+  assumes slices_ne:"\<And>p . p \<in> W \<Longrightarrow> slices p \<noteq> {}" \<comment> \<open>the set of slices of a well-behaved node is not empty\<close>
 begin
 
 definition quorum where
   "quorum Q \<equiv> \<forall> p \<in> Q \<inter> W . (\<exists> Sl \<in> slices p . Sl \<subseteq> Q)"
+  \<comment> \<open>A quorum is a set whose well-behaved members have at least one slice included in the set.\<close>
 
 end
 
-section \<open>The Cascade Theorem\<close>
+section \<open>Intact and the Cascade Theorem\<close>
 
 locale intact = \<comment> \<open>Here we fix an intact set @{term I} and prove the cascade theorem.\<close>
   orig:FBAS slices W 
-  + proj:FBAS "project slices I" W \<comment> \<open>We consider the projection of the system on @{term I}\<close>
+  + proj:FBAS "project slices I" W \<comment> \<open>We consider the projection of the system on @{term I}.\<close>
   for slices W I +
-  assumes "I \<subseteq> W" 
-    and q_avail:"orig.quorum I"
-    and q_inter:"\<And> Q Q' . \<lbrakk>proj.quorum Q; proj.quorum Q'; Q \<inter> I \<noteq> {}; Q' \<inter> I \<noteq> {}\<rbrakk>  \<Longrightarrow> Q \<inter> Q' \<inter> I \<noteq> {}"
+  assumes "I \<subseteq> W" \<comment> \<open>An intact set is a set @{term I} satisfying those three assumptions.\<close>
+    and q_avail:"orig.quorum I" \<comment> \<open>@{term I} is a quorum in the original system.\<close>
+    and q_inter:"\<And> Q Q' . \<lbrakk>proj.quorum Q; proj.quorum Q'; Q \<inter> I \<noteq> {}; Q' \<inter> I \<noteq> {}\<rbrakk>  \<Longrightarrow> Q \<inter> Q' \<inter> I \<noteq> {}" 
+    \<comment> \<open>Any two sets that intersect @{term I} and that are quorums in the projected system intersect in @{term I}.\<close>
 begin
 
 theorem cascade:
+\<comment> \<open>If @{term U} is a quorum of an intact node and @{term S} is a super-set of @{term U}, then either @{term S} includes 
+all intact nodes or there is an intact node outside of @{term S} which is blocked by the intact members of @{term S}.
+This shows that, in SCP, once the intact members of a quorum accept a statement, 
+a cascading effect occurs and all intact nodes eventually accept it regardless of what befouled and faulty nodes do.\<close>
   fixes U S
   assumes "orig.quorum U" and "U \<inter> I \<noteq> {}" and "U \<subseteq> S"
   obtains "I \<subseteq> S" | "\<exists> n \<in> I - S . \<forall> Sl \<in> slices n . Sl \<inter> S \<inter> I \<noteq> {}"
 proof -
-  have False if "\<forall> n \<in> I - S . \<exists> Sl \<in> slices n . Sl \<inter> S \<inter> I = {}" and "\<not>I \<subseteq> S"
+  have False if 1:"\<forall> n \<in> I - S . \<exists> Sl \<in> slices n . Sl \<inter> S \<inter> I = {}" and 2:"\<not>(I \<subseteq> S)"
   proof -
-    text \<open>First we show that @{term \<open>I-S\<close>} is a quorum in the projected system:\<close>
-    have "proj.quorum (I-S)" using that(1)
+    text \<open>First we show that @{term \<open>I-S\<close>} is a quorum in the projected system. This is immediate from the definition of quorum and assumption 1.\<close>
+    have "proj.quorum (I-S)" using 1
       unfolding proj.quorum_def project_def 
       by (auto; smt DiffI Diff_Compl Diff_Int_distrib Diff_eq Diff_eq_empty_iff Int_commute)
     text \<open>Then we show that U is also a quorum in the projected system:\<close>
@@ -45,7 +52,7 @@ proof -
       unfolding proj.quorum_def orig.quorum_def project_def 
       by (simp; meson Int_commute inf.coboundedI2)
     text \<open>Since quorums of @{term I} must intersect, we get a contradiction:\<close>
-    ultimately show False using \<open>U \<subseteq> S\<close> \<open>U \<inter> I \<noteq> {}\<close> \<open>\<not>I\<subseteq>S\<close> q_inter by auto
+    ultimately show False using \<open>U \<subseteq> S\<close> \<open>U \<inter> I \<noteq> {}\<close> \<open>\<not>(I\<subseteq>S)\<close> q_inter by auto
   qed
   thus ?thesis using that by blast
 qed
@@ -64,7 +71,7 @@ locale intersecting_intact =
 assumes inter:"I\<^sub>1 \<inter> I\<^sub>2 \<noteq> {}"
 begin
 
-theorem union_quorum: "i1.orig.quorum (I\<^sub>1\<union>I\<^sub>2)"
+theorem union_quorum: "i1.orig.quorum (I\<^sub>1\<union>I\<^sub>2)" \<comment> \<open>@{term \<open>I\<^sub>1\<union>I\<^sub>2\<close>} is a quorum in the original system.\<close>
   using i1.intact_axioms i2.intact_axioms
   unfolding  intact_def FBAS_def intact_axioms_def i1.orig.quorum_def
   by (metis Int_iff Un_iff le_supI1 le_supI2)
@@ -72,6 +79,7 @@ theorem union_quorum: "i1.orig.quorum (I\<^sub>1\<union>I\<^sub>2)"
 theorem union_quorum_intersection: 
   assumes "proj.quorum Q\<^sub>1" and "proj.quorum Q\<^sub>2" and "Q\<^sub>1 \<inter> (I\<^sub>1\<union>I\<^sub>2) \<noteq> {}" and "Q\<^sub>2 \<inter> (I\<^sub>1\<union>I\<^sub>2) \<noteq> {}"
   shows "Q\<^sub>1 \<inter> Q\<^sub>2 \<inter> (I\<^sub>1\<union>I\<^sub>2) \<noteq> {}"
+    \<comment> \<open>Any two sets that intersect @{term \<open>I\<^sub>1\<union>I\<^sub>2\<close>} and that are quorums in the system projected on @{term \<open>I\<^sub>1\<union>I\<^sub>2\<close>} intersect in @{term \<open>I\<^sub>1\<union>I\<^sub>2\<close>}.\<close>
 proof -
   text \<open>First we show that @{term Q\<^sub>1} and @{term Q\<^sub>2} are quorums in the projections on @{term I\<^sub>1} and @{term I\<^sub>2}.\<close>
   have "i1.proj.quorum Q\<^sub>1" using \<open>proj.quorum Q\<^sub>1\<close> 
