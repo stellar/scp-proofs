d4f668ac6307e096ee1e4cd1a471b7210ed5f9eb
Hidenori Shinohara <hidenori@stellar.org>
2021-01-29 14:56:32 +0000

    Set up a very, very simple test environment

---
+++ nomination-protocol/run_tests.sh
@@ -0,0 +1,25 @@
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+# This shell script simply checks if there is any output.
+# When the Ivy interpretor is not happy, it outputs something like
+# "network.ivy: line 52: error: assumption failed"
+# to stdout.
+# This shell script simply checks that.
+
+for shouldSucceed in tests/succeed/*; do
+    if [[ $(./nomination < $shouldSucceed) ]]; then
+        echo "${red}$shouldSucceed failed unexpectedly${reset}"
+    else
+        echo "${green}$shouldSucceed ran as expected${reset}"
+    fi
+done
+
+for shouldFail in tests/fail/*; do
+    if [[ $(./nomination < $shouldFail) ]]; then
+        echo "${green}$shouldFail failed as expected${reset}"
+    else
+        echo "${red}$shouldFail succeeded unexpectedly${reset}"
+    fi
+done
